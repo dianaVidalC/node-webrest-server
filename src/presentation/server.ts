c3435391b1bb78d3409974058f93672c74225cb3
Erika Vidal <erika.diana.vidal@gmail.com>
2025-10-17 23:20:05 -0500

    variable agregada

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -29,7 +29,7 @@ export class Server {
         });
 
         this.app.listen(3000, () => {
-            console.log('Server is running on port 3000');
+            console.log(`Server is running on port ${3000}`);
         });
 
     }
